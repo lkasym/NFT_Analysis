0e42d3ac8b01d04f40cf46cc2d8044d6737dbad3
lkasym <112753425+lkasym@users.noreply.github.com>
2023-10-26 23:50:57 +0530

    Update streamli_nft.py

--- streamli_nft.py
+++ streamli_nft.py
@@ -1,28 +1,68 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from tensorflow.keras.models import load_model
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+from sklearn.preprocessing import MinMaxScaler
 import pickle
 import matplotlib.pyplot as plt
 
-# Load the models and data
-lr_model = pickle.load(open("linear_regression_model.pkl", "rb"))
-lstm_model = load_model("lstm_model.h5")
-with open("lstm_scaler.pkl", "rb") as f:
-    lstm_scaler = pickle.load(f)
 
-data_path = "Processed_OpenSea_NFT_1_Sales.csv"  # Update with your path
+# Define the LSTM prediction function
+def predict_price_lstm(nft_name, data):
+    if nft_name not in data['asset.name'].values:
+        return None
+
+    def create_dataset(dataset, look_back=1):
+        X, Y = [], []
+        for i in range(len(dataset) - look_back - 1):
+            a = dataset[i:(i + look_back), 0]
+            X.append(a)
+            Y.append(dataset[i + look_back, 0])
+        return np.array(X), np.array(Y)
+
+    look_back = 1
+    nft_data = data[data['asset.name'] == nft_name].sort_values(by='sales_datetime')['price_in_ether'].values
+    nft_data = nft_data.reshape(-1, 1)
+    
+    if len(nft_data) <= look_back + 2:
+        return "Insufficient data for LSTM prediction"
+
+    train_size = int(len(nft_data) * 0.7)
+    train, test = nft_data[0:train_size,:], nft_data[train_size:len(nft_data),:]
+
+    if len(train) <= look_back + 2 or len(test) <= look_back + 2:
+        return "Insufficient data for LSTM prediction after splitting"
+
+    scaler = MinMaxScaler()
+    train = scaler.fit_transform(train)
+    test = scaler.transform(test)
+    
+    trainX, trainY = create_dataset(train, look_back)
+    testX, testY = create_dataset(test, look_back)
+    
+    trainX = np.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
+    testX = np.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
+    
+    model = Sequential()
+    model.add(LSTM(50, input_shape=(trainX.shape[1], trainX.shape[2])))
+    model.add(Dense(1))
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.fit(trainX, trainY, epochs=50, batch_size=1, verbose=1)
+    
+    testPredict = model.predict(testX)
+    testPredict = scaler.inverse_transform(testPredict)
+    
+    return testPredict[-1][0]
+
+
+# Load the Linear Regression model
+lr_model = pickle.load(open("linear_regression_model.pkl", "rb"))
+
+# Load the data
+data_path = "Processed_OpenSea_NFT_1_Sales.csv"
 nft_data = pd.read_csv(data_path)
 nft_data['price_in_ether'] = nft_data['total_price'] / 1e18
-
-# Data processing functions
-def create_dataset(dataset, look_back=1):
-    X, Y = [], []
-    for i in range(len(dataset) - look_back - 1):
-        a = dataset[i:(i + look_back), 0]
-        X.append(a)
-        Y.append(dataset[i + look_back, 0])
-    return np.array(X), np.array(Y)
 
 st.title("NFT Explorer and Price Predictor")
 
@@ -43,22 +83,15 @@ if nft_name:
         
         # Linear Regression Prediction
         if st.button("Predict Price with Linear Regression"):
-            price = lr_model.predict([[selected_nft['asset.num_sales'].iloc[0]]])  # Adjust based on your feature column
+            price = lr_model.predict([[selected_nft['asset.num_sales'].iloc[0]]])
             st.write(f"Predicted Price in Ether (using Linear Regression): {price[0]}")
-
         
         # LSTM Prediction
         if st.button("Predict Price with LSTM"):
-            selected_prices = selected_nft.sort_values(by='sales_datetime')['price_in_ether'].values
-            if len(selected_prices) > 2:  # Ensure there's enough data for prediction
-                selected_prices = selected_prices.reshape(-1, 1)
-                selected_prices_scaled = lstm_scaler.transform(selected_prices)
-                X_lstm, _ = create_dataset(selected_prices_scaled)
-                X_lstm = np.reshape(X_lstm, (X_lstm.shape[0], 1, X_lstm.shape[1]))
-                predicted_price_scaled = lstm_model.predict(X_lstm)
-                predicted_price = lstm_scaler.inverse_transform(predicted_price_scaled)
-                st.write(f"Predicted Price in Ether (using LSTM): {predicted_price[-1][0]}")
+            predicted_price = predict_price_lstm(nft_name, nft_data)
+            if isinstance(predicted_price, str):
+                st.write(predicted_price)
             else:
-                st.write("LSTM isn't applicable for this NFT due to insufficient transaction data.")
+                st.write(f"Predicted Price in Ether (using LSTM): {predicted_price}")
     else:
         st.write("NFT not found in the dataset.")
