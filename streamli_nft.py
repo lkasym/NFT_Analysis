84511430131f003698904d3bef0116d2912f69c2
lkasym <112753425+lkasym@users.noreply.github.com>
2023-10-27 04:00:03 +0530

    Update streamli_nft.py

--- streamli_nft.py
+++ streamli_nft.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
 from sklearn.preprocessing import MinMaxScaler
 import plotly.graph_objects as go
+import plotly.express as px
 import pickle
 
 # Load the models and data
@@ -15,7 +16,7 @@ data_path = "Processed_OpenSea_NFT_1_Sales.csv"
 nft_data = pd.read_csv(data_path)
 nft_data['price_in_ether'] = nft_data['total_price'] / 1e18
 
-
+# Function to create dataset for LSTM
 def create_dataset(dataset, look_back=1):
     X, Y = [], []
     for i in range(len(dataset) - look_back - 1):
@@ -24,49 +25,102 @@ def create_dataset(dataset, look_back=1):
         Y.append(dataset[i + look_back, 0])
     return np.array(X), np.array(Y)
 
+# Function to predict price using LSTM
 def predict_price_lstm(nft_name, data):
     if nft_name not in data['asset.name'].values:
         return None
 
     look_back = 1
-    nft_data = data[data['asset.name'] == nft_name].sort_values(by='sales_datetime')['price_in_ether'].values
-    nft_data = nft_data.reshape(-1, 1)
-    
-    if len(nft_data) <= look_back + 2:
+    nft_data_selected = data[data['asset.name'] == nft_name].sort_values(by='sales_datetime')['price_in_ether'].values
+    nft_data_selected = nft_data_selected.reshape(-1, 1)
+
+    if len(nft_data_selected) <= look_back + 2:
         return "Insufficient data for LSTM prediction"
 
-    train_size = int(len(nft_data) * 0.7)
-    train, test = nft_data[0:train_size,:], nft_data[train_size:len(nft_data),:]
-
-    if len(train) <= look_back + 2 or len(test) <= look_back + 2:
-        return "Insufficient data for LSTM prediction after splitting"
+    train_size = int(len(nft_data_selected) * 0.7)
+    train, test = nft_data_selected[0:train_size, :], nft_data_selected[train_size:len(nft_data_selected), :]
 
     scaler = MinMaxScaler()
     train = scaler.fit_transform(train)
     test = scaler.transform(test)
-    
+
     trainX, trainY = create_dataset(train, look_back)
     testX, testY = create_dataset(test, look_back)
-    
+
     trainX = np.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
     testX = np.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
-    
+
     model = Sequential()
     model.add(LSTM(50, input_shape=(trainX.shape[1], trainX.shape[2])))
     model.add(Dense(1))
     model.compile(loss='mean_squared_error', optimizer='adam')
     model.fit(trainX, trainY, epochs=50, batch_size=1, verbose=0)
-    
+
     testPredict = model.predict(testX)
     testPredict = scaler.inverse_transform(testPredict)
-    
+
     return testPredict[-1][0]
 
-st.title("NFT Explorer and Price Predictor")
+# Vibrant Styling
+st.markdown("""
+<style>
+    body {
+        color: #ffffff;
+        background-color: #341f97;
+        font-family: 'Helvetica Neue', sans-serif;
+    }
+    h1, h2 {
+        color: #fbcf02;
+    }
+    .stButton>button {
+        color: #4E8BF5;
+        background-color: #fbcf02;
+        border: none;
+        border-radius: 5px;
+    }
+    .sidebar .sidebar-content {
+        background-color: #8c62aa;
+    }
+    select, input {
+        border: none;
+        border-radius: 5px;
+    }
+</style>
+    """, unsafe_allow_html=True)
+nft_image_paths = ["DALL·E 2023-10-27 03.41.57 - Photo of a dreamy universe where countless stars form the shape of two faces gazing into each other's eyes. Around this celestial portrait, nebulas an.png",
+"DALL·E 2023-10-27 03.38.29 - Photo of a surreal library where books fly like birds, forming flocks that soar through an ethereal sky. Ladders twist and turn like DNA helixes, and .png",
+"DALL·E 2023-10-27 03.38.26 - Illustration of a cosmic art studio where planets serve as paint palettes and stars as glittering brush strokes. A female artist of Hispanic descent s.png",
+"DALL·E 2023-10-27 03.38.21 - Vector design of a digital time portal. Ancient structures like pyramids and coliseums merge with futuristic cities and spaceships. Diverse human silh.png",
+"DALL·E 2023-10-27 03.36.20 - Illustration of a digital neural network branching out like a tree, with nodes glowing in vibrant colors. At the base is a human brain, symbolizing th.png",
+"DALL·E 2023-10-27 03.35.50 - Oil painting of a serene cybernetic garden. Mechanical plants with LED flowers sway gently, and robotic butterflies flutter around. A male and female .png",
+"DALL·E 2023-10-27 03.33.47 - Oil painting of an overhead view of a cricket ground in India during sunset. The stadium lights are on, casting a golden hue on the field. Diverse pla.png",
+"DALL·E 2023-10-27 03.33.42 - Vector design of a cricket ball and bat intertwined with digital elements, symbolizing the fusion of traditional sport and modern technology. Surround.png",
+"DALL·E 2023-10-27 03.31.47 - Vector design of two chat bubbles against a starry night. Inside one bubble is a picturesque scene of Pune with the silhouette of the 19-year-old male.png",
+"DALL·E 2023-10-27 03.25.54 - Vector design of a broken digital clock, its numbers glitching and shifting erratically. The background is a chaotic swirl of dark colors, and fractur.png",
+"DALL·E 2023-10-27 03.25.50 - Oil painting of a desolate urban landscape post-apocalypse. Buildings are in ruins, the sky is overcast with dark clouds, and the streets are flooded.png",
+"DALL·E 2023-10-27 03.24.34 - Vector design of a steampunk-inspired mechanical heart, intricately detailed with gears, pipes, and valves. It pulsates with electric energy and is co.png",
+"DALL·E 2023-10-27 03.24.30 - Watercolor painting of a serene beach where the sand is pixelated and the waves form a digital pattern. In the distance, a lighthouse beams a WiFi sig.png",
+"DALL·E 2023-10-27 03.24.26 - Photo of a futuristic cityscape at dusk with neon lights reflecting on the water and flying cars zooming past skyscrapers. People of various descents .png",
+]
 
+# Main Function
+def main():
+    st.title("Welcome to NFT Explorer and Price Predictor!")
+    st.write("""
+    **Disclaimer:** This model is trained on data from 2019-2021 and might not be accurate for the current date.
+    """)
 
-# NFT Lookup
-nft_name = st.text_input("Enter NFT name:")
+    menu = ["Home", "Price Predictor", "Market Analysis", "User/Trader Analysis", "NFT Categories", "NFT Gallery"]
+    choice = st.selectbox("Menu", menu)
+
+    if choice == "Home":
+        st.write("""
+        Explore various functionalities. Choose from Price Predictor, Market Analysis, User/Trader Analysis, NFT Categories, and NFT Gallery.
+        """)
+
+    elif choice == "Price Predictor":
+        st.subheader("Price Predictor")
+        nft_name = st.text_input("Enter NFT name:")
 
 if nft_name:
     selected_nft = nft_data[nft_data['asset.name'] == nft_name]
@@ -114,26 +168,24 @@ if nft_name:
     else:
         st.write("NFT not found in the dataset.")
 
-# ... [Rest of the code for other options like "Market Analysis", "User/Trader Analysis", etc.]
-
-
-elif selection == "Market Analysis":
-   
-    st.header("Market Analysis: Collections in Demand")
+    elif choice == "Market Analysis":
+        st.subheader("Market Analysis")
+          st.header("Market Analysis: Collections in Demand")
     
     # Count sales by collection
     collections = nft_data[nft_data['asset.collection.name'] != 'uncategorized'].groupby('asset.collection.name').size().sort_values(ascending=False).head(10)
     fig = px.bar(collections, title='Top 10 Collections by Sales Volume')
     st.plotly_chart(fig)
 
-
-elif selection == "User/Trader Analysis":
-    st.write("User/Trader Analysis")
+    elif choice == "User/Trader Analysis":
+        st.subheader("User/Trader Analysis")
+         # Most active sellers
+    top_sellers = nft_data['seller.user.username'].value_counts().  st.header("Market Analysis: Collections in Demand")
     
-   
-    
-    # Most active sellers
-    top_sellers = nft_data['seller.user.username'].value_counts().head(10)
+    # Count sales by collection
+    collections = nft_data[nft_data['asset.collection.name'] != 'uncategorized'].groupby('asset.collection.name').size().sort_values(ascending=False).head(10)
+    fig = px.bar(collections, title='Top 10 Collections by Sales Volume')
+    st.plotly_chart(fig)head(10)
     st.write("Most Active Sellers:")
     fig = go.Figure(data=[go.Bar(x=top_sellers.index, y=top_sellers.values)])
     st.plotly_chart(fig)
@@ -150,9 +202,25 @@ elif selection == "User/Trader Analysis":
     fig = go.Figure(data=[go.Bar(x=buyer_names, y=top_buyers.values)])
     st.plotly_chart(fig)
 
-elif selection == "NFT Categories":
-    # NFT Categories Code
-    st.write("NFT Categories")
+
+    elif choice == "NFT Categories":
+        st.subheader("NFT Categories")
+        st.write("NFT Categories")
     categories = nft_data['Category'].value_counts()
     fig = go.Figure(data=[go.Pie(labels=categories.index, values=categories.values)])
     st.plotly_chart(fig)
+
+    elif choice == "NFT Gallery":
+        st.subheader("NFT Gallery")
+        st.write("Explore a curated selection of 10-15 NFTs!")
+         
+    
+    # Displaying the images in a grid format
+    col1, col2, col3 = st.beta_columns(3)
+    
+    for index, image_path in enumerate(nft_image_paths):
+        with [col1, col2, col3][index % 3]:
+            st.image(image_path, use_column_width=True, caption=f"NFT {index+1}")
+
+if __name__ == "__main__":
+    main()
